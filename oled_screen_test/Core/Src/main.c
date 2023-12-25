4f80fd42da3fbe42334c8858e39b654945bfb1ec
Pritham13 <prithamhsuvarna@gmail.com>
2023-12-25 13:01:24 +0530

    updating oled_test

--- oled_screen_test/Core/Src/main.c
+++ oled_screen_test/Core/Src/main.c
@@ -24,6 +24,8 @@
 #include "ssd1306.h"
 #include "fonts.h"
 #include "test.h"
+#include "bitmap.h"
+#include "horse_anim.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -91,13 +93,109 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+  SSD1306_Init();  // initialise
 
+  /// lets print some string
+
+    SSD1306_GotoXY (0,0);
+    SSD1306_Puts ("HELLO", &Font_11x18, 1);
+    SSD1306_GotoXY (10, 30);
+    SSD1306_Puts ("  WORLD :)", &Font_11x18, 1);
+    SSD1306_UpdateScreen(); //display
+
+    HAL_Delay (2000);
+
+
+    SSD1306_ScrollRight(0,7);  // scroll entire screen
+    HAL_Delay(2000);  // 2 sec
+
+    SSD1306_ScrollLeft(0,7);  // scroll entire screen
+    HAL_Delay(2000);  // 2 sec
+
+    SSD1306_Stopscroll();
+    SSD1306_Clear();
+
+    SSD1306_DrawBitmap(0,0,logo, 128, 64, 1);
+    SSD1306_UpdateScreen();
+
+    HAL_Delay(2000);
+
+    SSD1306_ScrollRight(0x00, 0x0f);    // scroll entire screen right
+
+    HAL_Delay (2000);
+
+    SSD1306_ScrollLeft(0x00, 0x0f);  // scroll entire screen left
+
+    HAL_Delay (2000);
+
+    SSD1306_Scrolldiagright(0x00, 0x0f);  // scroll entire screen diagonal right
+
+    HAL_Delay (2000);
+
+    SSD1306_Scrolldiagleft(0x00, 0x0f);  // scroll entire screen diagonal left
+
+    HAL_Delay (2000);
+
+    SSD1306_Stopscroll();   // stop scrolling. If not done, screen will keep on scrolling
+
+
+    SSD1306_InvertDisplay(1);   // invert the display
+
+    HAL_Delay(2000);
+
+    SSD1306_InvertDisplay(0);  // normalize the display
+//
+
+    HAL_Delay(2000);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse1,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse2,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse3,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse4,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse5,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse6,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse7,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse8,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse9,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
+
+	  	  SSD1306_Clear();
+	  	  SSD1306_DrawBitmap(0,0,horse10,128,64,1);
+	  	  SSD1306_UpdateScreen();
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
