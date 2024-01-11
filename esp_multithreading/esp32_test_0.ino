a6e666afa339edec4d6b83422981b46a265b36f3
Pritham13 <prithamhsuvarna@gmail.com>
2024-01-11 23:16:32 +0530

    updating files

--- esp_multithreading/esp32_test_0.ino
+++ esp_multithreading/esp32_test_0.ino
@@ -1,13 +1,21 @@
+//implementation of queue left
 #include <Arduino_FreeRTOS.h>
 #include <queue.h>
 #include "DHT.h"
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 Adafruit_BMP085 bmp;
 
 #define DHTPIN 4   
 #define DHTTYPE DHT11
+#define SCREEN_WIDTH 128 // OLED display width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// declare an SSD1306 display object connected to I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 //defining struct
 typedef struct TelemteryData
@@ -20,21 +28,24 @@ typedef struct TelemteryData
 
 TelemteryData data_acquired; //declating struct
 
-QueueHandle_t arrayQueue;
+QueueHandle_t struct_queue;
 
 void setup(){
+    if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        }
     aquired_data_queue=xQueueCreate(5, //Queue length
                             sizeof(int));
     if (aquired_data_queue != NULL){
         xTaskCreate(Task_hum_temp_read,
                     "collection_of_humidity_and_temperature_data",
-                    129,//needs to be changed 
+                    129,//stack size//needs to be changed 
                     NULL,
-                    2,//priority //needs to be decided
+                    1,//priority //needs to be decided
                     NULL);
         xTaskCreate(Task_value_display,
                     "displaying_of_the_collected_values",
-                    129,//needs to be changed 
+                    129,//stack size//needs to be changed 
                     NULL,
                     2,//priority //needs to be decided
                     NULL);
@@ -51,12 +62,40 @@ void Task_hum_temp_read(void *pvParameters){
         data_arquired.temperature = bmp.readTemperature();
         data_arquired.altitude = bmp.readAltitude();
 
+
         
     }
 }
 void Task_value_display(void *pvParameters){
     (void)pvParameters;
     for(;;){
+        oled.clearDisplay();
+
+        char display_buffer[20];
+
+        // Display Humidity
+        sprintf(display_buffer, "Humidity: %.2f %%", humidity);
+        oled.setCursor(0, 10);
+        oled.println(display_buffer);
+
+        // Display Temperature
+        sprintf(display_buffer, "Temp: %.2f C", temperature);
+        oled.setCursor(0, 24);
+        oled.println(display_buffer);
+
+        // Display Pressure
+        sprintf(display_buffer, "Pressure: %.2f hPa", pressure);
+        oled.setCursor(0, 36);
+        oled.println(display_buffer);
+
+        // Display Altitude
+        sprintf(display_buffer, "Altitude: %.2f m", altitude);
+        oled.setCursor(0, 48);
+        oled.println(display_buffer);
+
+        oled.display();
+
+        delay(2000); // Adjust delay as needed
         
     }
 }
