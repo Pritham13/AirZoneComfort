48f6fe3fad8c94a9ca0cc6ec8454a9ce741fa1cd
Pritham13 <prithamhsuvarna@gmail.com>
2024-02-06 23:25:44 +0530

    updating files

--- prediction/prediction_test.py
+++ prediction/prediction_test.py
@@ -2,27 +2,30 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-model = tf.keras.models.load_model('C:/Users/prit4/OneDrive/Desktop/stuff/active_Github_repos/Mini_project/Training/model.h5')
+def predict_fan_speed(manual_values):
+    model = tf.keras.models.load_model('model.h5')
+
+    scaled_features = pd.DataFrame({
+        'month': [manual_values['month'] / 12],
+        'day': [manual_values['day'] / 31],
+        'time': [manual_values['time'] / 24],
+        'humidity': [manual_values['humidity'] / 100],
+        'tempC': [manual_values['tempC'] / 50]
+    })
+
+    predictions = model.predict(scaled_features)
+
+    predicted_class = np.argmax(predictions)
+
+    return predicted_class
 
 manual_values = {
-    'month': 6,  
-    'day': 21,   
-    'time': 24,  
-    'humidity': 90,
-    'tempC': 30   
+    'month': 6,  # Example month value
+    'day': 15,   # Example day value
+    'time': 12,  # Example time value
+    'humidity': 80,  # Example humidity value
+    'tempC': 25   # Example temperature value
 }
 
-# Preprocess the manual input
-scaled_features = pd.DataFrame({
-    'month': [manual_values['month'] / 12],
-    'day': [manual_values['day'] / 31],
-    'time': [manual_values['time'] / 24],
-    'humidity': [manual_values['humidity'] / 100],
-    'tempC': [manual_values['tempC'] / 50]
-})
-
-predictions = model.predict(scaled_features)
-
-predicted_class = np.argmax(predictions)
-
-print("Predicted Fan Speed Class:", predicted_class)
+predicted_fan_speed = predict_fan_speed(manual_values)
+print("Predicted Fan Speed Class:", predicted_fan_speed)
